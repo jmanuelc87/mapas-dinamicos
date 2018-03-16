5004b60fea4fe8cbaa2479adbb1880fd5ca9082b
Juan Manuel Carballo <jm.carb@gmail.com>
2018-03-15 18:33:03 -0600

    Cambios en el proyecto para compilar la libreria ESRI y demo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import * as Map from 'esri/Map';
+import * as MapView from 'esri/views/MapView';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+    @ViewChild('map')
+    private mapViewEl: ElementRef;
+
+    ngOnInit(): void {
+        const map: __esri.Map = new Map({
+            basemap: 'oceans'
+        });
+
+        const view: __esri.MapView = new MapView({
+            map: map,
+            container: this.mapViewEl.nativeElement,
+            center: [-112, 38],
+            zoom: 6
+        });
+    }
 }
