1a2a99b84f0a9d20c764248cf19fdb2159627857
Servicio Social DSG <ssocial.dsg@siap.local>
2018-05-29 11:17:46 -0500

    Agregadas propiedades del componente

--- src/app/components/modalidad/modalidad.component.ts
+++ src/app/components/modalidad/modalidad.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { v4 as uuid } from "uuid";
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-modalidad',
@@ -10,6 +11,12 @@ export class ModalidadComponent implements OnInit {
 
   @Input()
   private id;
+
+  @Input()
+  private group: FormGroup;
+
+  @Input()
+  private name: string;
 
   constructor() { }
 
