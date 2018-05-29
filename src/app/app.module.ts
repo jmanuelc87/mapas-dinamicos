c08e6a6aaa6e478bdfa1000482a16ae2fe55e3ba
Servicio Social DSG <ssocial.dsg@siap.local>
2018-05-29 11:20:24 -0500

    cambios en las carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { CicloComponent } from './components/form/ciclo/ciclo.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { EsriMapComponent } from './components/esri-map/esri-map.component';
 import { HeaderComponent } from './components/header/header.component';
-import { ModalidadComponent } from './components/modalidad/modalidad.component';
+import { ModalidadComponent } from './components/form/modalidad/modalidad.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { CicloComponent } from './components/ciclo/ciclo.component';
 
 
 @NgModule({
