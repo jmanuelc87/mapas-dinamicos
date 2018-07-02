ef4c2402ea91a9407695c8b4eedbbdf33f19f71d
Servicio Social DSG <ssocial.dsg@siap.local>
2018-07-02 15:07:43 -0500

    agregado funcionalidad al menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,41 +6,61 @@ import {
     OnInit,
     Output,
     Renderer2,
-    ViewChild
+    ViewChild,
+    OnDestroy
 } from '@angular/core';
+import { visitSiblingRenderNodes } from '@angular/core/src/view/util';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
-    private isIEOrEdge = /msie\s|trident\/|edge\//i.test(window.navigator.userAgent);
-
-    leftMenu = {
-        'marginLeft': '0',
-    };
+export class HeaderComponent implements OnInit, OnDestroy {
 
     @Output()
     private menuClick: EventEmitter<string> = new EventEmitter();
+
+    private unsubcriber = [];
 
     constructor(
         private renderer: Renderer2
     ) { }
 
     ngOnInit() {
+        this.hideAllMenus();
 
-        console.log(this.isIEOrEdge);
+        let elements = document.getElementsByClassName('dropdown-toggle');
 
-        if (this.isIEOrEdge) {
-            this.leftMenu = {
-                'marginLeft': '-110px',
-            };
+        for (let i = 0; i < elements.length; i++) {
+            this.unsubcriber.push(this.renderer.listen(elements[i], 'click', (event) => {
+                event.stopPropagation();
+                let sibling = this.renderer.nextSibling(event.target);
+
+                if (sibling.style.display == 'none') {
+                    this.renderer.setStyle(sibling, 'display', 'inherit');
+                } else {
+                    this.renderer.setStyle(sibling, 'display', 'none');
+                }
+            }));
+        }
+
+        this.renderer.listen(document, 'click', (event) => {
+            event.stopPropagation();
+            this.hideAllMenus();
+        });
+    }
+
+    hideAllMenus() {
+        let elements = document.getElementsByClassName('d-menu');
+        for (let i = 0; i < elements.length; i++) {
+            this.renderer.setStyle(elements[i], 'display', 'none');
         }
     }
 
-    onClick(event) {
-        this.menuClick.emit(event.target.innerText);
+    ngOnDestroy(): void {
+        for (let i = 0; i < this.unsubcriber.length; i++) {
+            this.unsubcriber[i]();
+        }
     }
 }
